4b7878b14648be2e756d26b487bd6a56d633196d
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-05-25 23:30:20 +0600

    first commit

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+
+const MainLayout = ({children}) => {
+    return (
+        <div>
+            <Navbar/>
+            {children}
+        </div>
+    );
+};
+
+export default MainLayout;
