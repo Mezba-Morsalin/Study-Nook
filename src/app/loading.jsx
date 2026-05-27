04b103d328ab7d4170610c887e278df768d111ad
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-05-27 18:39:41 +0600

    Created Loading Page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { MoonLoader } from 'react-spinners';
 
 const loading = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#071228]">
+      <MoonLoader color="#FFD700" size={50} />
+      <p className='mt-3 bg-linear-to-r from-[#FFD700] via-[#FFC107] to-[#FFB300] bg-clip-text text-transparent font-bold'>Loading...</p>
+    </div>
     );
 };
 
